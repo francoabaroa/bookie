7d54234374da586e447a30ee35ff59be241d4538
Franco Abaroa <franco9395@gmail.com>
2019-02-11 14:13:12 -0800

    [nit] Change double quotes to single quotes in edit book page

--- client/src/components/EditBook.js
+++ client/src/components/EditBook.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import '../App.css';
 
-import stringHash from "string-hash";
+import stringHash from 'string-hash';
 
 import {
   fetchBooksAction,
